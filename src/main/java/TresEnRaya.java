459fde460a223534e55ffdbdc709bb2b69b05742
Fulano2141 <macuto2142dt@gmail.com>
2021-04-26 21:04:35 -0400

    More Tests

--- src/main/java/TresEnRaya.java
+++ src/main/java/TresEnRaya.java
@@ -1,18 +1,80 @@
 public class TresEnRaya {
     private final int MAX = 3;
-    private final int MIN = 3;
-    private int[][] tablero = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0}
+    private final int MIN = 1;
+    private int iteracion = 0;
+    private Character[][] tablero = {
+            {'0', '0', '0'},
+            {'0', '0', '0'},
+            {'0', '0', '0'}
     };
+    private char ultimoJugador = '+';
 
-    public void jugar(int x, int y) throws Exception {
-        if (x > MAX || x < 0 || y > MAX || y < 0)
-            throw new RuntimeException("X salio de rango");
-        if (tablero[x - 1][y - 1] != 0)
+    public String jugar(int x, int y) throws Exception {
+        verificarLimites(x, y);
+        if (tablero[x - 1][y - 1] != '0')
             throw new Exception("Casilla ocupada");
-        tablero[x - 1][y - 1] = 1;
+        if (ultimoJugador == '+')
+            tablero[x - 1][y - 1] = 'X';
+        else
+            tablero[x - 1][y - 1] = '+';
 
+        ultimoJugador = siguienteTurno();
+        print2D(getTablero());
+        iteracion++;
+        return "No hay ganador";
+    }
+
+    private void verificarLimites(int x, int y) {
+        if (x > getMAX() || x < getMIN() || y > getMAX() || y < getMIN())
+            throw new RuntimeException("X o Y salio de rango");
+    }
+
+    public int getIteracion() {
+        return iteracion;
+    }
+
+    public void setIteracion(int iteracion) {
+        this.iteracion = iteracion;
+    }
+
+    public char getUltimoJugador() {
+        return ultimoJugador;
+    }
+
+    public void setUltimoJugador(char ultimoJugador) {
+        this.ultimoJugador = ultimoJugador;
+    }
+
+    public int getMAX() {
+        return MAX;
+    }
+
+    public int getMIN() {
+        return MIN;
+    }
+
+
+    public Character[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Character[][] tablero) {
+        this.tablero = tablero;
+    }
+
+    private static void print2D(Character[][] mat) {
+        for (Character[] characters : mat) {
+            for (Character character : characters)
+                System.out.print(character + " ");
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+    public char siguienteTurno() {
+        if (ultimoJugador == 'X')
+            return '+';
+        return 'X';
     }
 }
--- src/test/java/TresEnRayaTest.java
+++ src/test/java/TresEnRayaTest.java
@@ -1,19 +1,24 @@
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class TresEnRayaTest {
-
+    private TresEnRaya tresEnRaya;
     @Rule
     public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void before() {
+        tresEnRaya = new TresEnRaya();
+    }
 
     @Test
     public void cuandoFueraXLanzarException() throws Exception {
         //PARTES DE UNA PRUEBA Preparacion // Logica // Verificacion
-        TresEnRaya tresEnRaya = new TresEnRaya();
-//        exception.expect(Exception.class);
+        // exception.expect(Exception.class);
         exception.expect(RuntimeException.class);
         tresEnRaya.jugar(5, 2);
         tresEnRaya.jugar(-1, 2);
@@ -21,16 +26,58 @@ public class TresEnRayaTest {
 
     @Test
     public void cuandoFueraYLanzarException() throws Exception {
-        TresEnRaya tresEnRaya = new TresEnRaya();
         exception.expect(RuntimeException.class);
         tresEnRaya.jugar(2, 5);
         tresEnRaya.jugar(2, -5);
     }
+
     @Test
     public void cuandoCasillaOcupadaLanzarException() throws Exception {
-        TresEnRaya tresEnRaya = new TresEnRaya();
-        tresEnRaya.jugar(2, 1);
+        tresEnRaya.jugar(2, 1); // X
         exception.expect(Exception.class);
+        tresEnRaya.jugar(2, 1); // +
+    }
+
+    @Test
+    public void siPrimerTurnoEntoncesJuegaX() throws Exception {
+        assertEquals('X', tresEnRaya.siguienteTurno());
+    }
+
+    @Test
+    public void siPrimerTurnoXSiguiente() throws Exception {
         tresEnRaya.jugar(2, 1);
+        assertEquals('+', tresEnRaya.siguienteTurno());
+        tresEnRaya.jugar(1, 2);
+        assertEquals('X', tresEnRaya.siguienteTurno());
+        tresEnRaya.jugar(3, 3);
+        assertEquals('+', tresEnRaya.siguienteTurno());
+        System.out.println(tresEnRaya.getIteracion());
+    }
+
+    @Test
+    public void cuandoJuegaNoHayGanador() throws Exception {
+        String resultado = tresEnRaya.jugar(2, 1);
+        assertEquals("No hay ganador", resultado);
+    }
+
+    @Test
+    public void siLlenaUnEjeVerticalEntoncesGana() throws Exception {
+        tresEnRaya.jugar(1, 1);
+        tresEnRaya.jugar(1, 2);
+        tresEnRaya.jugar(2, 1);
+        tresEnRaya.jugar(2, 2);
+        String resultado = tresEnRaya.jugar(3, 1);
+        assertEquals("X es el ganador", resultado);
+    }
+
+    @Test
+    public void siLlenaUnEjeHorizontalEntoncesGana() throws Exception {
+        tresEnRaya.jugar(2, 1);
+        tresEnRaya.jugar(1, 1);
+        tresEnRaya.jugar(3, 1);
+        tresEnRaya.jugar(1, 2);
+        tresEnRaya.jugar(2, 2);
+        String resultado = tresEnRaya.jugar(1, 3);
+        assertEquals("X es el ganador", resultado);
     }
 }
